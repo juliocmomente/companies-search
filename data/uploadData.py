a4ad55b57103959d5082b636377e3af99466bb86
Davi-Muta <davimassaru@hotmail.com>
2024-04-30 13:10:15 -0300

    gpt engine

---
+++ data/uploadData.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import iris
+
+df = pd.read_csv('/opt/irisbuild/data/glassdoor_reviews.csv')
+
+#pros_embeddings = model.encode(df['pros'].tolist(), normalize_embeddings=True)
+#df['pros_vector'] = pros_embeddings.tolist()
+
+#cons_embeddings = model.encode(df['cons'].tolist(), normalize_embeddings=True)
+#df['cons_vector'] = cons_embeddings.tolist()
+
+
+sql = f"""
+                CREATE TABLE reviews (
+                    firm VARCHAR(255),
+                    jobTitle VARCHAR(255),
+                    pros VARCHAR(200000),
+                    cons VARCHAR(200000)
+                )
+            """
+result = iris.sql.exec(sql)
+
+for index, row in df.iterrows():
+    try:
+        #stmt = iris.sql.prepare("INSERT INTO reviews (firm, jobTitle, pros, cons) VALUES (?, ?, ?, ?)") FIND python ERROR  irisbuiltins.SQLError: <UNIMPLEMENTED>term+84^%qaqpslx
+        #https://docs.intersystems.com/irislatest/csp/docbook/DocBook.UI.Page.cls?KEY=AFL_epython#AFL_epython_pylibrary
+
+        glassdoor_review = iris.cls("User.reviews")._New()
+        glassdoor_review.firm = row['firm']
+        glassdoor_review.jobTitle = row['job_title']
+        glassdoor_review.pros = row['pros']
+        glassdoor_review.cons = row['cons']
+        glassdoor_review._Save()
+
+    except Exception as ex:
+        print(ex)
